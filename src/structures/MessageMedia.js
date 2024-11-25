989b7244f73949de72e215732491be336c84fe3d
Vladson  Pontes <vlad.pontes@gmail.com>
2024-11-25 15:43:49 -0300

    changing mime import

--- src/structures/MessageMedia.js
+++ src/structures/MessageMedia.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const path = require('path');
-const mime = require('mime');
+const mime = await import('mime');
 const fetch = require('node-fetch');
 const { URL } = require('url');
 
